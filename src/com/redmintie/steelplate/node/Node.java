455f0336621429020078a55b4a5e01fbaeeb1af8
Joshua Minter <josh@minteronline.com>
2015-11-01 17:51:21 +1000

    Added some extra abilities to the node class.

--- src/com/redmintie/steelplate/node/Node.java
+++ src/com/redmintie/steelplate/node/Node.java
@@ -1,20 +1,36 @@
-package com.redmintie.steelplate;
+package com.redmintie.steelplate.node;
 
 import java.util.ArrayList;
 
+import com.redmintie.steelplate.math.Point;
 import com.redmintie.steelplate.render.Canvas;
 
 public class Node {
+	public Point position = new Point();
+	public double angle;
+	public int width;
+	public int height;
 	private ArrayList<Node> children = new ArrayList<Node>();
+	public boolean testOverlap(Node other) {
+		return position.getX() - width / 2 < other.position.getX() + other.width / 2
+				|| position.getX() + width / 2 > other.position.getX() - other.width / 2
+				|| position.getY() - height / 2 < other.position.getY() + other.height / 2
+				|| position.getY() + height / 2 > other.position.getY() - other.height / 2;
+	}
 	public void update(double delta) {
 		for (Node child : children) {
 			child.update(delta);
 		}
 	}
 	public void draw(Canvas canvas) {
+		canvas.pushMatrix();
+		canvas.translate(position.getX(), position.getY());
+		canvas.rotate(angle);
+		canvas.translate(-width / 2, -height / 2);
 		for (Node child : children) {
 			child.draw(canvas);
 		}
+		canvas.popMatrix();
 	}
 	public void end() {
 		for (Node child : children) {
---
+++ src/demo/Demo.java
@@ -0,0 +1,24 @@
+package demo;
+import com.redmintie.steelplate.core.Game;
+import com.redmintie.steelplate.render.Canvas;
+
+public class Demo extends Game {
+	public static void main(String[] args) {
+		new Demo();
+	}
+	public Demo() {
+	}
+	@Override
+	public void init() {
+	}
+	@Override
+	public void update(double delta) {
+	}
+	@Override
+	public void draw(Canvas canvas) {
+	}
+	@Override
+	public void close() {
+		end();
+	}
+}
