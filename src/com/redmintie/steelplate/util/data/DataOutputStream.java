2a4406ebf6f4a491dc2215b9e3a595429c367ac1
Joshua Minter <josh@minteronline.com>
2015-11-28 18:14:45 +1000

    Finished data reading/writing
     - definitely last commit for tonight. Goodnight (again)

--- src/com/redmintie/steelplate/util/data/DataOutputStream.java
+++ src/com/redmintie/steelplate/util/data/DataOutputStream.java
@@ -1,5 +1,20 @@
 package com.redmintie.steelplate.util.data;
 
-public class DataOutputStream {
+import java.io.IOException;
+import java.io.OutputStream;
 
+public class DataOutputStream implements AutoCloseable {
+	private java.io.DataOutputStream stream;
+	public DataOutputStream(OutputStream stream) {
+		this.stream = new java.io.DataOutputStream(stream);
+	}
+	public void writeDataObject(DataObject object) throws IOException {
+		stream.writeLong(object.getHeader());
+		stream.writeShort(object.getSize());
+		object.writeData(stream);
+	}
+	@Override
+	public void close() throws IOException {
+		stream.close();
+	}
 }
--- src/com/redmintie/steelplate/util/data/DataStorage.java
+++ src/com/redmintie/steelplate/util/data/DataStorage.java
@@ -2,6 +2,7 @@ package com.redmintie.steelplate.util.data;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class DataStorage {
@@ -17,6 +18,11 @@ public class DataStorage {
 		in.readDataObject(result);
 		in.close();
 		return result;
+	}
+	public static void writeObject(String name, DataObject object) throws IOException {
+		DataOutputStream out = new DataOutputStream(new FileOutputStream(getFile(name)));
+		out.writeDataObject(object);
+		out.close();
 	}
 	public static void deleteObject(String name) {
 		getFile(name).delete();
